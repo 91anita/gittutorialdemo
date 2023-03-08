bf324b1eb160fa9c2e9663b798beda432012a89c
91anita <anitasahani91>
2023-03-08 17:48:50 +0530

    hide

--- demo.js
+++ demo.js
@@ -1,8 +1,8 @@
 var li=document.getElementsByTagName('li');
 console.log(li);
 console.log(li[2]);
-li[2].style.backgroundColor='green'
-for(var i=0; i<=li.length; i++)
-{
-    li[i].style.fontWeight='bold';
-}
+li[1].style.backgroundColor='green'
+
+var invisible= document.querySelector('.list-group-item:nth-child(3)')
+invisible.style.visibility='hidden'
+

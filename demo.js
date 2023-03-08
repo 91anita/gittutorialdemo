8da11bacd134b44b258c4487f93b8a8eeb730f46
91anita <anitasahani91>
2023-03-08 15:13:09 +0530

    newadd

--- demo.js
+++ demo.js
@@ -1,1 +1,5 @@
-console.log("I love git")
+var items=document.getElementsByClassName('title');
+console.log(items)
+console.log(items[0])
+items[0].style.fontweight='bold';
+items[0].style.backgroundColor='green';

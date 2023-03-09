df630c29bfee2a3abd6b52453d372750a5ebb9bd
91anita <anitasahani91>
2023-03-09 23:39:01 +0530

    deletebtn

--- demo.js
+++ demo.js
@@ -1,8 +1,8 @@
 
 function store(event){
     event.preventDefault();
-    const Name=event.target.name.value;
-    const Email=event.target.email.value;
+    const Name=event.target.usernm.value;
+    const Email=event.target.emailid.value;
     localStorage.setItem('Name', Name)
     localStorage.setItem('Email', Email)
 const obj={
@@ -16,6 +16,14 @@ function showOnScreen(obj){
     const parentElement=document.getElementById('users')
     var childElement=document.createElement('li')
     childElement.textContent=obj.Name+'-'+obj.Email
+    const deleteButton=document.createElement('input')
+    deleteButton.type="button"
+    deleteButton.value='Delete'
+    deleteButton.onclick=()=>{
+        localStorage.removeItem(obj.Email)
+        parentElement.removeChild(childElement)
+    }
+    childElement.appendChild(deleteButton)
     parentElement.appendChild(childElement)
      //parentElement.innerHTML = parentElement.innerHTML+ <li>${obj.Name} - ${obj.Email}</li>
 }
